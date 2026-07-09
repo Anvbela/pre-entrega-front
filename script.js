3aca2fda332ec0e9b4d288c5d7de7a3e257f9f84
Anvbela <coscarellianabela@gmail.com>
2026-07-09 19:44:55 -0300

    agregar cambios

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+const productos = document.querySelector(".productos");
+const carrito = document.querySelector(".agregar-carrito");
+const precios = document.querySelector(".precios");
+const boton = document.getElementById("hamburguesa");
+const menu = document.getElementById("menu");
+
+boton.addEventListener("click", (hamburguesa) => {
+
+    menu.classList.toggle("activo");
+
+})
